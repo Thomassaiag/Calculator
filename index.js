8fb80cecb0eed9ead6a6d0a8a8ad7688798aea41
Thomassaiag <thomas.saiag@gmail.com>
2023-05-16 22:53:32 +0200

    + and - working with case switch

--- index.js
+++ index.js
@@ -3,30 +3,27 @@ const buttons=document.querySelectorAll("input[class=buttonClass]")
 const buttonEqual=document.querySelector(".buttonClassEqual")
 let inputInt=[]
 let inputOperateur=[]
-console.log(buttons)
-
 
 for (i=0;i<buttons.length;i++){
     buttons[i].addEventListener("click",function(){
         let spliter= /\*|\+|\-|\//
-        console.log(this.value)
-        console.log(parseInt(this.value))
-        console.log("click detected")
+        // console.log(this.value)
+        // console.log(parseInt(this.value))
+        // console.log("click detected")
         if(screen.innerHTML==""){
-            console.log("if")
+            // console.log("if")
             screen.innerHTML=this.value
         }
         else{
-            console.log("else")
+            // console.log("else")
             screen.innerHTML+=this.value
         }
         if (isNaN(parseInt(this.value))){
             inputOperateur.push(this.value)
         }
-        // inputInt=screen.innerHTML.split("+").join(',').split("-").join(',').split("/").join(',').split("*")
         inputInt=screen.innerHTML.split(spliter)
-        console.log(inputInt)
-        console.log(inputOperateur)
+        // console.log(inputInt)
+        // console.log(inputOperateur)
 
     })
 }
@@ -34,16 +31,25 @@ for (i=0;i<buttons.length;i++){
 
 
 buttonEqual.addEventListener("click",function(){
-    let result=0
-    console.log(inputInt)
-    for (let element of inputInt){
-        console.log('boucle')
-        console.log(element)
-        console.log(parseInt(element))
-        result+=parseInt(element)
-        
+    let result=parseInt(inputInt[0])
+    // console.log(inputInt)
+    // console.log(result)
+    for (i=1;i<inputInt.length;i++){
+        // console.log('boucle')
+        // console.log(parseInt(inputInt[i]))
+        operateur=inputOperateur[parseInt(i)-1]
+        // console.log(operateur)
+        switch(operateur){
+            case "+":
+            result+=parseInt(inputInt[i])
+            break
+            case "-":
+            result-=parseInt(inputInt[i])
+            break
+        }
+        // console.log(`result=${result}`)
     }
-    console.log(result)
+    // console.log(result)
     screen.innerHTML=""
     screen.innerHTML=result
 })
