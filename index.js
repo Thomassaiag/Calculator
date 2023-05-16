18a3cd38fda9d9f265d6cc7409e8b76be6bd6d22
Thomassaiag <thomas.saiag@gmail.com>
2023-05-16 22:24:23 +0200

    split management with regexp

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const screen=document.getElementById("screen")
-const buttons=document.querySelectorAll("input[class^=buttonClass]")
-
+const buttons=document.querySelectorAll("input[class=buttonClass]")
+const buttonEqual=document.querySelector(".buttonClassEqual")
+let inputInt=[]
+let inputOperateur=[]
 console.log(buttons)
 
 
 for (i=0;i<buttons.length;i++){
     buttons[i].addEventListener("click",function(){
+        let spliter= /\*|\+|\-|\//
         console.log(this.value)
+        console.log(parseInt(this.value))
         console.log("click detected")
         if(screen.innerHTML==""){
             console.log("if")
@@ -16,5 +20,30 @@ for (i=0;i<buttons.length;i++){
             console.log("else")
             screen.innerHTML+=this.value
         }
+        if (isNaN(parseInt(this.value))){
+            inputOperateur.push(this.value)
+        }
+        // inputInt=screen.innerHTML.split("+").join(',').split("-").join(',').split("/").join(',').split("*")
+        inputInt=screen.innerHTML.split(spliter)
+        console.log(inputInt)
+        console.log(inputOperateur)
+
     })
 }
+
+
+
+buttonEqual.addEventListener("click",function(){
+    let result=0
+    console.log(inputInt)
+    for (let element of inputInt){
+        console.log('boucle')
+        console.log(element)
+        console.log(parseInt(element))
+        result+=parseInt(element)
+        
+    }
+    console.log(result)
+    screen.innerHTML=""
+    screen.innerHTML=result
+})
